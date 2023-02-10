70fce1db56ba957e8b32c6b3cd17a2515b53bad3
Colton Wilson <colton_wilson93@yahoo.com>
2023-02-09 17:03:44 -0800

    Small fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 //Skeleton code from Module 14 lesson 20 /controllers/index.js
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 
 router.use('/api', apiRoutes);
 
